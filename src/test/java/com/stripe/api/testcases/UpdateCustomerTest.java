cbfbe10fc1b76feb225887908d0c6be5852c1451
Srinivasa Hebbar <srinivasa.hebbar@gmail.com>
2021-03-18 11:15:36 +0530

    second commit

---
+++ src/test/java/com/stripe/api/testcases/UpdateCustomerTest.java
@@ -0,0 +1,11 @@
+package com.stripe.api.testcases;
+
+import com.stripe.api.commonUtiles.BaseClass;
+
+public class UpdateCustomerTest extends BaseClass{
+
+	
+	public void testCommit() {
+		System.out.println("Commit testing");
+	}
+}
